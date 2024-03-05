55594ceb6e7916554756b10b1df9548ea937c46e
parul@1507 <parul@joni.co.in>
2024-03-05 10:01:47 +0530

    chnage data

--- backend/src/controllers/diamondController.js
+++ backend/src/controllers/diamondController.js
@@ -1,40 +1,40 @@
 const catchAsyncError = require("../utils/catchAsyncError")
 const Diamond = require("../models/diamondModel")
-const ApiFeatures =require("../utils/apiFeatures")
-const Contact =require("../models/contactModel")
+const ApiFeatures = require("../utils/apiFeatures")
+const Contact = require("../models/contactModel")
 
 module.exports = {
-searchDiamond: catchAsyncError(async (req, res, next) => {
-    console.log(" req.body", req.body)
-    const { shape, color ,polish,cut,clarity,sym,fluo,lab,eyeClean,location,fromSize,toSize} = req.body;
+    searchDiamond: catchAsyncError(async (req, res, next) => {
+        console.log(" req.body 12333333333333333", req.body)
+        const { shape, color, polish, cut, clarity, sym, fluo, lab, eyeClean, location, fromSize, toSize } = req.body
 
-        let addquery =[]
+        let addquery = []
         if (color) {
-            addquery.push({ Color: { $in:color} })
+            addquery.push({ Color: { $in: color } })
         }
         if (fromSize && toSize) {
-            addquery.push({ Weight: { $gt:fromSize,$lt:toSize} })
+            addquery.push({ Weight: { $gt: fromSize, $lt: toSize } })
         }
         if (shape) {
-            addquery.push({ Shape: { $in:shape } })
+            addquery.push({ Shape: { $in: shape } })
         }
         if (polish) {
-            addquery.push({ Polish: { $in:polish } })
+            addquery.push({ Polish: { $in: polish } })
         }
         if (cut) {
-            addquery.push({ Cut: { $in:cut } })
+            addquery.push({ Cut: { $in: cut } })
         }
         if (clarity) {
-            addquery.push({ Clarity: { $in:clarity } })
+            addquery.push({ Clarity: { $in: clarity } })
         }
         if (sym) {
-            addquery.push({ Sym: { $in:sym } })
+            addquery.push({ Sym: { $in: sym } })
         }
         if (fluo) {
-            addquery.push({ Fl: { $in:fluo} })
+            addquery.push({ Fl: { $in: fluo } })
         }
         if (lab) {
-            addquery.push({ Lab: { $in:lab } })
+            addquery.push({ Lab: { $in: lab } })
         }
         // if (eyeClean) {
         //     addquery.push({ eyeClean: eyeClean })
@@ -43,7 +43,7 @@ searchDiamond: catchAsyncError(async (req, res, next) => {
             addquery.push({ LOCATION: location })
         }
 
-            console.log("addquery",JSON.stringify(addquery,2,null))
+        console.log("addquery", JSON.stringify(addquery, 2, null))
         let arrData
         if (addquery.length > 0) {
             arrData = {
@@ -53,15 +53,10 @@ searchDiamond: catchAsyncError(async (req, res, next) => {
             arrData = {}
         }
 
-        console.log("addquery",JSON.stringify(arrData))
+        console.log("addquery", JSON.stringify(arrData))
         const totalCount = await Diamond.find(arrData).countDocuments()
-        
-        const apiFeature = new ApiFeatures(
-            Diamond.find(arrData).sort({SRNo: 1 }),
-            req.query
-        )
-            .search()
-            .filter()
+
+        const apiFeature = new ApiFeatures(Diamond.find(arrData).sort({ SRNo: 1 }), req.query).search().filter()
 
         let result = await apiFeature.query
 
@@ -78,27 +73,27 @@ searchDiamond: catchAsyncError(async (req, res, next) => {
             filterCount: filterCount ? filterCount : 0,
         }
         return res.status(200).json({ status: 1, message: "all version list", data: resultData })
-}),
-getDiamondDetails : catchAsyncError(async (req, res, next) => {
-    console.log("------",req.params.id)
-    const result = await Diamond.findById(req.params.id);
+    }),
+    getDiamondDetails: catchAsyncError(async (req, res, next) => {
+        console.log("------", req.params.id)
+        const result = await Diamond.findById(req.params.id)
 
-    res.status(200).json({
-        success: 1,
-        result,
-    });
-}),
-addContact : catchAsyncError(async (req, res, next) => {
-    const {message,name,email} = req.body
-    const result = await Contact.create({
-        message,
-        name,
-        email
-    });
+        res.status(200).json({
+            success: 1,
+            result,
+        })
+    }),
+    addContact: catchAsyncError(async (req, res, next) => {
+        const { message, name, email } = req.body
+        const result = await Contact.create({
+            message,
+            name,
+            email,
+        })
 
-    res.status(200).json({
-        success: 1,
-        result,
-    });
-}),
+        res.status(200).json({
+            success: 1,
+            result,
+        })
+    }),
 }
